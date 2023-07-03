1ab6a0371c7ba63f3274bf789b94e0e35a628eb2
Roma <romagupta701@gmail.com>
2023-07-03 15:37:34 +0530

    Product test has been done

--- CoreLogic/Services/ProductService.cs
+++ CoreLogic/Services/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoreLogic.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -9,13 +10,14 @@ namespace CoreLogic.Services
 {
     public class ProductService
     {
-        void getAllProduct()
+        public List<Product> getAllProduct()
         {
             var products = new List<Product>()
             {
                 new Product(){ Id = 1, Category = "Mobile", Name = 'One Plus', Description = "6GB RAM, 64GB storage", Price = 20000, Quantity = 2},
                 new Product(){ Id = 2, Category = "Laptop", Name = 'MSI', Description = "8GB RAM, 512GB storage (SSD)", Price = 20000, Quantity = 2}
-            }
+            };
+            return products;
         }
     }
 }
---
+++ CoreTests/ProductTests.cs
@@ -0,0 +1,31 @@
+﻿using CoreLogic.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreTests
+{
+    public class ProductTests
+    {
+        ProductService productService;
+        [SetUp]
+
+        public void Setup()
+        {
+            productService = new ProductService();
+        }
+
+        [Test]
+        public void GetAllProductWorkAsExpected()
+        {
+            // call the service
+            var res = productService.getAllProduct();
+            var count = res.Count();
+
+            // make sure that the count is not zero
+            Assert.IsTrue(count >0);
+        }
+    }
+}
