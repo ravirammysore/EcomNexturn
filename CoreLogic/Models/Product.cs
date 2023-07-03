73bde8e3e6709d500a48d2b2ed60edfac45bfec3
Ravi K R <raviram.mysore@gmail.com>
2023-07-03 20:20:23 +0530

    Product and its service fixed

--- CoreLogic/Models/Product.cs
+++ CoreLogic/Models/Product.cs
@@ -8,13 +8,10 @@ namespace CoreLogic.Models
 {
     public class Product
     {
-        public int Id { get; set; }
-        public string Category { get; set; }
-
+        public int Id { get; set; }        
         public string Name { get; set; }
         public string Description { get; set; }
-        public int Price { get; set; }
-        public int Quantity { get; set; }
+        public int Price { get; set; }       
 
     }
 }
--- CoreLogic/Services/ProductService.cs
+++ CoreLogic/Services/ProductService.cs
@@ -14,8 +14,20 @@ namespace CoreLogic.Services
         {
             var products = new List<Product>()
             {
-                new Product(){ Id = 1, Category = "Mobile", Name = 'One Plus', Description = "6GB RAM, 64GB storage", Price = 20000, Quantity = 2},
-                new Product(){ Id = 2, Category = "Laptop", Name = 'MSI', Description = "8GB RAM, 512GB storage (SSD)", Price = 20000, Quantity = 2}
+                new Product()
+                { 
+                    Id = 1, 
+                    Name = "One Plus", 
+                    Description = "6GB RAM, 64GB storage", 
+                    Price = 20000,
+                },
+                new Product()
+                { 
+                    Id = 2, 
+                    Name = "MSI", 
+                    Description = "8GB RAM, 512GB storage (SSD)", 
+                    Price = 20000
+                }
             };
             return products;
         }
