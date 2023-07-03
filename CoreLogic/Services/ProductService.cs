b845c3946fd51247b2608da86a146c50e3ceb23a
Indresh <indresh.nitjsr@gmail.com>
2023-07-03 15:16:44 +0530

    productServices added

---
+++ CoreLogic/Services/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLogic.Services
+{
+    public class ProductService
+    {
+        void getAllProduct()
+        {
+            var products = new List<Product>()
+            {
+                new Product(){ Id = 1, Category = "Mobile", Name = 'One Plus', Description = "6GB RAM, 64GB storage", Price = 20000, Quantity = 2},
+                new Product(){ Id = 2, Category = "Laptop", Name = 'MSI', Description = "8GB RAM, 512GB storage (SSD)", Price = 20000, Quantity = 2}
+            }
+        }
+    }
+}
