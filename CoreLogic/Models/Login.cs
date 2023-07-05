235256e5f2670aaadf573784707129368922de75
Sushobhit Kumar Singh <shobhit2259@gmail.com>
2023-07-05 12:53:30 +0530

    Login Dode

--- CoreLogic/Models/Login.cs
+++ CoreLogic/Models/Login.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace CoreLogic.Models
 {
-    internal class Login
+    public class Login
     {
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
